c25a91cf3678f701370ae5fc6cd983056ac2bd0d
Vladimir Burdukov <chippcheg@gmail.com>
2022-01-09 22:38:36 +0200

    Added anyhow for prettier error presentation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use anyhow::Result;
 use futures_executor::block_on;
 use gitext::handle;
-use gitext::Error;
 
-fn main() -> Result<(), Error> {
-    block_on(handle(std::env::args()))
+fn main() -> Result<()> {
+    block_on(handle(std::env::args()))?;
+    Ok(())
 }
